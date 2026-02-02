f7feca88eecc91160098276e6584192ac46ee519
goyallucky <luckygoyal221982@gmail.com>
2026-02-02 11:47:25 +0530

    [Lucky] Adds main entry point to run all Reflection programs

---
+++ Reflection/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection.Metadata;
+namespace Reflection;
+using CalculatorReflection;
+using DynamicMethodInvocation;
+using ReflectionExample;
+using RetrieveAttributes;
+using StaticFieldReflection;
+using StudentReflection;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ConfigMain.Execute();
+        // AttributeMain.Execute();
+        // MathMain.Execute();
+        // StudentMain.Execute();
+        // CalculatorMain.Execute();
+        // ReflectionMain.Execute();
+        // ClassInfoReflection.Execute();
+    }
+}
